7639cfe19fe030065334e0fb964d9b32682de7ad
a22347328 <a22347328@gmail.com>
2025-01-22 05:29:53 +0800

    Initial Commit

---
+++ SwiftUI_Movie/SwiftUI_MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MovieApp.swift
+//  SwiftUI_Movie
+//
+//  Created by User on 2025/1/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
